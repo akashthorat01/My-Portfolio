73d6b336808bb6c6567ef2908f6b1a853023ff8e
akashathorat <akashathorat2020@gmail.com>
2024-02-05 14:29:44 +0530

    feat: added  new features

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
   var options = {
-    strings: ["Frontend Developer", "Backend Developer","Web Developer","Java Developer","Web Designer"],
+    strings: ["Frontend Developer...", "Backend Developer...","Web Developer...","Java Developer...","Web Designer..."],
     typeSpeed: 70,  // typing speed in milliseconds
     backSpeed: 25,  // backspacing speed in milliseconds
     showCursor: true,  // whether to show the blinking cursor
